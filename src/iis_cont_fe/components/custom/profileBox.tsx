5a5c9ad5dc9aa162a9bf7337c09b3ba45beaf85d
artem_vereninov <duboshit50@gmail.com>
2024-11-23 22:30:41 +0100

    smth

--- src/iis_cont_fe/components/custom/profileBox.tsx
+++ src/iis_cont_fe/components/custom/profileBox.tsx
@@ -1,5 +1,26 @@
 import { Avatar, AvatarImage, AvatarFallback } from '@radix-ui/react-avatar';
 import React from 'react';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuPortal,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from "@/components/ui/button"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { Separator } from "@/components/ui/separator"
+
+const tags = Array.from({ length: 50 }).map(
+    (_, i, a) => `v1.2.0-beta.${a.length - i}`
+)
 
 const CenteredAvatar: React.FC = () => {
     return (
@@ -8,6 +29,26 @@ const CenteredAvatar: React.FC = () => {
                 <AvatarImage src="https://picsum.photos/seed/picsum/200/300" alt="Avatar" />
                 <AvatarFallback>JL</AvatarFallback>
             </Avatar>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="outline">Open</Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                    <ScrollArea className="h-72 w-48 rounded-md border">
+                        <div className="p-4">
+                            <h4 className="mb-4 text-sm font-medium leading-none">Tags</h4>
+                            {tags.map((tag, index) => (
+                                <>
+                                    <div key={index} className="text-sm">
+                                        {tag}
+                                    </div>
+                                    <Separator className="my-2" />
+                                </>
+                            ))}
+                        </div>
+                    </ScrollArea>
+                </DropdownMenuContent>
+            </DropdownMenu>
         </div>
     );
 };
