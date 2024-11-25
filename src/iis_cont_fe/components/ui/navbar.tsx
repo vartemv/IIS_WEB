a361ff593de76b72a89682065208829e9588ac48
artem_vereninov <duboshit50@gmail.com>
2024-11-25 21:33:41 +0100

    fixed layout

--- src/iis_cont_fe/components/ui/navbar.tsx
+++ src/iis_cont_fe/components/ui/navbar.tsx
@@ -2,30 +2,22 @@ import React from 'react';
 import { useUser } from '@/hooks/useUser';
 
 const Navbar: React.FC = () => {
-  const {user} = useUser();
+  const { user } = useUser();
   const homePath = `/profile/${user?.user.profileName}`;
   return (
-    <nav className="flex justify-between items-center bg-gray-800 text-white p-4">
-      <div className="flex-shrink-0">
+    <nav className="flex flex-col items-center bg-gray-800 h-16 text-white ">
+      <div className="flex-shrink-0 absolute top-4 left-4">
         <a href="/info" className="text-xl font-bold text-white">
           FITstagram
         </a>
       </div>
-      <div className="flex-grow">
-        <ul className="flex space-x-4 justify-center list-none m-0 p-0">
-          <li>
-            <a href={user ? homePath : "/info"} className="text-white hover:underline">
-              Home
-            </a>
-          </li>
-          <li>
-          </li>
-          <li>
-            <a href="/groups" className="text-white hover:underline">
-              Groups
-            </a>
-          </li>
-        </ul>
+      <div className="flex-grow flex flex-row items-center justify-center gap-6">
+          <a href={user ? homePath : "/info"} className="text-white hover:underline">
+            Home
+          </a>
+          <a href="/groups" className="text-white hover:underline">
+            Groups
+          </a>
       </div>
     </nav>
   );
